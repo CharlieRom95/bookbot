5f413025a75fce02602f33b11b7097bba8bcee6c
CharlieRom <cromerovalarino@gmail.com>
2025-01-28 13:00:37 -0400

    cree funcion de contar caracteres

--- main.py
+++ main.py
@@ -3,12 +3,22 @@ def count_words(book_to_count):
     
     return len(list_with_words)
 
+def count_characters(book_to_count):
+    new_dict = {}
+    new_book_to_count = book_to_count.lower()
+    for character in new_book_to_count:
+        new_dict[character] = 0
+    for character in new_book_to_count:
+        new_dict[character] += 1
+    return new_dict
+
+
 
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-    number_of_words = count_words(file_contents)
-    print(number_of_words)
+    number_of_characters = count_characters(file_contents)
+    print(number_of_characters)
     
 
 
